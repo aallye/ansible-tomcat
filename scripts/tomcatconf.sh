81b735c60ac995cd73bfda1f70b35305ded8229f
anton <anton@ecsc001042b0.epam.com>
2016-04-11 12:19:47 +0300

    160411

--- scripts/tomcatconf.sh
+++ scripts/tomcatconf.sh
@@ -3,6 +3,7 @@
 export work_folder=/var/local/ansible/tomcatsrc/apache-tomcat-9.0.0.M4-src
 export CATALINA_HOME=$work_folder/output/build
 sed -i 's/Connector port=\"8080\"/Connector port=\"8083\"/g' $work_folder/output/build/conf/server.xml
+chmod -Rf g+w $work_folder/output/
 #cp -f $work_folder/conf/tomcat-users.xml $work_folder/output/build/conf/tomcat-users.xml
 #echo '<user username="anton" password="tomcat" roles="admin-gui,manager-gui,manager-script"/>' >> $work_folder/output/build/conf/tomcat-users.xml
 #echo '<user username="upload" password="upload" roles="manager-gui,manager-script"/>' >> $work_folder/output/build/conf/tomcat-users.xml
